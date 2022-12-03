5a6ce16d098dece23a4ef8913385dc0cf33bb8a4
sharad9201 <sharad.rae19@gmail.com>
2022-12-03 23:07:15 +0545

    crud

--- Desktop/vue-rest_api/src/App.vue
+++ Desktop/vue-rest_api/src/App.vue
@@ -4,16 +4,16 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-
-    <div class="wrapper">
-
+    <div class="max-w-7xl mx-auto">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink class="px-4 py-2 mr-4 bg-indigo-600 hover:bg-indigo-800 rounded text-white" to="/">Home</RouterLink>
+        <RouterLink to="/skills">Skill</RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+<main class="max-w-7xl mx-auto min-h-screen">
+<RouterView />
+</main>
 </template>
 
